a64f6e04be2f57f875e6363fdf4ebb530f926266
Francisco Gomez <mail@frncscgmz.me>
2015-02-25 09:44:47 -0800

    Included constructor and one stat on main gem file to use as a Gem

--- lib/bbmetric.rb
+++ lib/bbmetric.rb
@@ -3,3 +3,17 @@ require "colorize"
 
 require_relative "bbmetric/cli"
 require_relative "bbmetric/statcalc"
+
+class BBMetric
+
+   attr_accessor :precision
+
+   def initialize(precision = 3)
+      @precision = precision
+   end
+
+   def battingaverage(hits,atbats)
+      (BBmetric::StatCalc.battingaverage(hits,atbats))
+         .round(precision) if precision
+   end
+end
